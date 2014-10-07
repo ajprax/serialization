97c1fa9c8bd9501bf292e6d5fca71b08c5576f8c
Aaron Feldstein <afeldstein@wibidata.com>
2014-10-06 22:23:23 -0700

    Basic pom.xml

---
+++ api/src/main/java/org/ajprax/serialization/schema/FieldType.java
@@ -0,0 +1,25 @@
+package org.ajprax.serialization.schema;
+
+public enum FieldType {
+  UNSIGNED_8,
+  UNSIGNED_16,
+  UNSIGNED_32,
+  UNSIGNED_64,
+  UNSIGNED_BIG,
+  SIGNED_8,
+  SIGNED_16,
+  SIGNED_32,
+  SIGNED_64,
+  SIGNED_BIG,
+  FLOAT_32,
+  FLOAT_64,
+  FLOAT_BIG,
+  BOOLEAN,
+  STRING,
+  EXTENSION,
+  ENUM,
+  ARRAY,
+  SET,
+  MAP,
+  UNION
+}
